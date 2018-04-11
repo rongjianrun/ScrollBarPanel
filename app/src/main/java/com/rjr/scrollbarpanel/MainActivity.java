44ce8bad215bff91d6c85f5ad8c978d24cf428bc
rongjianrun <674807275@qq.com>
2018-04-12 00:27:45 +0800

    逻辑完善

--- app/src/main/java/com/rjr/scrollbarpanel/MainActivity.java
+++ app/src/main/java/com/rjr/scrollbarpanel/MainActivity.java
@@ -2,12 +2,78 @@ package com.rjr.scrollbarpanel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.rjr.scrollbarpanel.widget.ScrollBarListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ScrollBarListView mSbListView;
+    private List<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSbListView = ((ScrollBarListView) findViewById(R.id.sb_list_view));
+        initData();
+        mSbListView.setAdapter(new MyAdapter(data));
+    }
+
+    private void initData() {
+        data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data.add(String.valueOf(i));
+        }
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        private List<String> list;
+
+        public MyAdapter(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, parent, false);
+                holder = new ViewHolder();
+                holder.tv = (TextView) convertView.findViewById(R.id.tv);
+                convertView.setTag(holder);
+            }
+            holder = (ViewHolder) convertView.getTag();
+            holder.tv.setText(list.get(position));
+            return convertView;
+        }
+    }
+
+    class ViewHolder {
+        private TextView tv;
     }
 }
--- app/src/main/java/com/rjr/scrollbarpanel/widget/ScrollBarListView.java
+++ app/src/main/java/com/rjr/scrollbarpanel/widget/ScrollBarListView.java
@@ -3,9 +3,13 @@ package com.rjr.scrollbarpanel.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AbsListView;
 import android.widget.ListView;
 
@@ -22,23 +26,45 @@ public class ScrollBarListView extends ListView implements AbsListView.OnScrollL
     private int mWidthMeasureSpec;
     private int mHeightMeasureSpec;
     private int mScrollBarPanelPosition; // 定义滑动条y坐标位置
-
-    public ScrollBarListView(Context context) {
-        this(context, null);
-    }
+    private int thumbOffset; // 指示器在ListView中y轴的高度
+    private Animation mInAnimation; // 气泡控件显示时的动画
+    private Animation mOutAnimation; // 气泡控件隐藏时的动画
 
     public ScrollBarListView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public ScrollBarListView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        super(context, attrs);
+        super.setOnScrollListener(this);
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ScrollBarListView);
         int layoutId = ta.getResourceId(R.styleable.ScrollBarListView_scrollBarPanel, -1);
         int inAnimId = ta.getResourceId(R.styleable.ScrollBarListView_scrollBarPanelInAnim, -1);
         int outAnimId = ta.getResourceId(R.styleable.ScrollBarListView_scrollBarPanelOutAnim, -1);
         ta.recycle();
         setScrollBarPanel(layoutId);
+        initAnimations(inAnimId, outAnimId);
+    }
+
+    private void initAnimations(int inAnimId, int outAnimId) {
+        mInAnimation = AnimationUtils.loadAnimation(getContext(), inAnimId);
+        mOutAnimation = AnimationUtils.loadAnimation(getContext(), outAnimId);
+        int scrollBarFadeDuration = ViewConfiguration.getScrollBarFadeDuration();
+        mOutAnimation.setDuration(scrollBarFadeDuration);
+        mOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (mScrollBarPanel != null) {
+                    mScrollBarPanel.setVisibility(GONE);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     @Override
@@ -92,12 +118,24 @@ public class ScrollBarListView extends ListView implements AbsListView.OnScrollL
 
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (mScrollBarPanel == null) {
+            return;
+        }
         // 不断计算mScrollBarPanelPosition的值
         /*
-         * computeHorizontalScrollExtent() 滑动条在纵向滑动范围内经过放大后自身的高度
-         * computeHorizontalScrollOffset() 滑动条纵向幅度的位置 中间--5000
-         * computeHorizontalScrollRange() 滑动的范围：0--10000
+         * computeVerticalScrollExtent() 滑动条在纵向滑动范围内经过放大后自身的高度
+         * computeVerticalScrollOffset() 滑动条纵向幅度的位置 中间--5000
+         * computeVerticalScrollRange() 滑动的范围：0--10000
          */
+        // 1.计算系统滑块的高度
+        // 思路：滑块高度 / ListView高度 = computeVerticalScrollExtent() / computeVerticalScrollRange()
+        int height = Math.round(computeVerticalScrollExtent() * getMeasuredHeight() * 1.0f / computeVerticalScrollRange());
+        // 2.得到系统滑块中间的y坐标
+        // 思路：height / computeVerticalScrollExtent() = thumbOffset / computeVerticalScrollOffset()
+        thumbOffset = Math.round(height * computeVerticalScrollOffset() * 1.0f / computeVerticalScrollExtent());
+        thumbOffset += height / 2;
+        // 得到气泡控件顶部的y轴坐标
+        mScrollBarPanelPosition = thumbOffset - mScrollBarPanel.getMeasuredHeight() / 2;
         // 不断地控制气泡View的重新摆放
         int left = getMeasuredWidth() - mScrollBarPanel.getMeasuredWidth() - getVerticalScrollbarWidth();
         mScrollBarPanel.layout(
@@ -106,17 +144,36 @@ public class ScrollBarListView extends ListView implements AbsListView.OnScrollL
                 left + mScrollBarPanel.getMeasuredWidth(),
                 mScrollBarPanelPosition + mScrollBarPanel.getMeasuredHeight());
     }
+
+    /**
+     * 唤醒scrollBar回调
+     * @param startDelay
+     * @param invalidate
+     * @return
+     */
+    @Override
+    protected boolean awakenScrollBars(int startDelay, boolean invalidate) {
+        boolean awaken = super.awakenScrollBars(startDelay, invalidate);
+        if (mScrollBarPanel == null) {
+            return awaken;
+        }
+        if (awaken) {
+            if (mScrollBarPanel.getVisibility() == GONE) {
+                mScrollBarPanel.setVisibility(VISIBLE);
+                mScrollBarPanel.startAnimation(mInAnimation);
+            }
+            handler.removeCallbacks(r);
+            handler.postDelayed(r, 100);
+        }
+        return awaken;
+    }
+
+    Handler handler = new Handler();
+
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            mScrollBarPanel.startAnimation(mOutAnimation);
+        }
+    };
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
